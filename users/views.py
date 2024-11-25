9d2cd5366d171b5dac3f43932c8007eabe64a527
Nuno Cardoso <pv29105@estgl.ipv.pt>
2024-11-25 15:16:00 +0000

    Adicionei

---
+++ users/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+
+# Create your views here.
+# Create your views here.
+# Create your views here.
+# Create your views here.
+# Create your views here.
+# Create your views here.
+# Create your views here.
+# Create your views here.
+# Create your views here.
+# Create your views here.
+# Create your views here.
+# Create your views here.
+# Create your views here.
+# Create your views here.
+# Create your views here.
+# Create your views here.
+# Create your views here.
