aa81a8afa067fe8596078bbe703417fcee194e7f
Rafael29105 <nuno05954@gmail.com>
2024-12-10 17:59:22 +0000

    Delete Restaurante directory

--- Restaurante/Restaurante/menu/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.menu, name='menu'),
-    path('adicionar/', views.adicionar_prato, name='adicionar_prato'),
-    path('editar/<int:pk>/', views.editar_prato, name='editar_prato'),
-    path('excluir/<int:pk>/', views.excluir_prato, name='excluir_prato'),
-]
